d454289ac8ad5ea162e17ac2a9f6dc2987a01900
asus <sashmlv@yandex.ru>
2020-10-12 11:37:06 +0300

    Fix service start.

--- lib/commands/service_start.sh
+++ lib/commands/service_start.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+# start service if no exists process found
+
 function service_start {
 
-   systemctl enable "$name.service"
-   systemctl start "$name.service"
-   systemctl daemon-reload
+   previous_pid=$(cat 2>/dev/null "$pid_file")
+
+   if ! kill -0 -"$previous_pid" 2>/dev/null; then
+      systemctl enable "$name.service"
+      systemctl start "$name.service"
+      systemctl daemon-reload
+   fi
+
    exit
 }
--- lib/commands/service_stop.sh
+++ lib/commands/service_stop.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# stop process quiet
+
 function service_stop {
 
+   previous_pid=$(cat 2>/dev/null "$pid_file")
+   kill -- -"$previous_pid" 2>/dev/null
    systemctl stop "$name.service"
    systemctl daemon-reload
    exit
